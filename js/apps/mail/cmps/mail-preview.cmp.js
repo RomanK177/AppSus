ed04815bbf37bb4ad07248a559b31b9bbbd7a8f0
RomanK <romanklebanov17@gmail.com>
2020-11-04 18:58:49 +0200

    sfg

--- js/apps/mail/cmps/mail-preview.cmp.js
+++ js/apps/mail/cmps/mail-preview.cmp.js
@@ -3,9 +3,8 @@ export default {
     template: `
         <!-- <section class="mail-preview" :class="previewClass"> -->
         <section class="mail-preview" >
-           <h4>FFFFF</h4>
            <h4>{{mail.subject}}</h4>
-           <h5>Body: {{mail.body}}</h5>
+           <p>{{mail.body}}</p>
            <!-- <router-link :to="'/car/' +car.id " exact>Details</router-link> -->
            <!-- <router-link :to="'/car/edit/' +car.id " exact>Edit</router-link> -->
         </section>
