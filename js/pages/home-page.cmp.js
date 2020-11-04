57f2612a9e072725933bb3399393da515baad457
RomanK <romanklebanov17@gmail.com>
2020-11-04 13:17:20 +0200

    ss

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,13 +1,13 @@
-import { myRouter } from "../routes.js";
+// import { myRouter } from "../routes.js";
 export default {
-    router: myRouter,
+    // router: myRouter,
     template: `
     <section class="homepage">
         <div class="homepage-txt">
             <h1>Welcome to the Home Page!</h1>
             <p>Your favorite new book app! Where you can find books in our library, read about them, rate them, and buy them!</p>
             <router-link to="/notes" >Notes App</router-link>
-            <router-view></router-view>
+            <!-- <router-view></router-view> -->
             <p>Enjoy!</p>
         </div>
     </section>
