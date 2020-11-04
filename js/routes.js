bd512a27d0f4f61129247904d35f1ea8f9f3acef
RomanK <romanklebanov17@gmail.com>
2020-11-04 16:10:45 +0200

    s

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import homePage from "./pages/home-page.cmp.js";
-import notesApp from './apps/note/pages/note-app.cmp'
-import mailApp from './apps/mail/pages/mail-app.cmp'
-=======
 import homePage from './pages/home-page.cmp.js'
 import noteApp from './apps/note/pages/note-app.cmp.js'
 import mailApp from './apps/mail/pages/mail-app.cmp.js'
->>>>>>> 5d7bf5e1dcc63147bdb205cd3737cd9dbe75f310
 
 const myRoutes = [{
         path: "/",
